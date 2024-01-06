cc8be24ad96794f868898c944e9644a67055499f
turbo159 <turbo159@outlook.com>
2024-01-06 15:35:10 -0600

    update

--- pihole-start.sh
+++ pihole-start.sh
@@ -1,3 +1,11 @@
+echo "Getting the latest version of pihole/pihole..."
+docker pull pihole/pihole
+
+echo "Stopping pihole..."
+docker stop pihole
+
 # Docker compose startup...
-Echo $"Staring pihole using docker-compose..."
+echo "Staring pihole using the latest image via docker-compose..."
 docker compose --file pihole-compose.yaml up -d
+
+echo "Done."
